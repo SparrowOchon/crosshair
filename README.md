c4e8771414d8bd6f58fd4841b8d60de86cb806ec
Network Silence <tshah688@gmail.com>
2019-11-15 22:44:25 -0500

    Updated to allow SIGINT and draw at correct height

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # crosshair
-PyQT5 snippet for running overlaying crosshair on top of every window
+
+PyQT5 snippet for running overlaying crosshair on top of every window.
 
 # Instructions
+
 ```bash
 pip install pyqt5
 
-git clone https://github.com/georgegach/crosshair.git
+git clone https://github.com/SparrowOchon/crosshair.git
 cd crosshair/
 python cross.py
 ```
--- cross.py
+++ cross.py
@@ -1,17 +1,26 @@
 import sys
+import signal
 from PyQt5 import QtCore, QtGui, QtWidgets
 
+
 class Crosshair(QtWidgets.QWidget):
-    def __init__(self, parent=None, windowSize=24, penWidth=2):
+    def __init__(self, parent=None, windowSize=24, penWidth=5):
         QtWidgets.QWidget.__init__(self, parent)
         self.ws = windowSize
-        self.resize(windowSize+1, windowSize+1)
-        self.pen = QtGui.QPen(QtGui.QColor(0,255,0,255))                
-        self.pen.setWidth(penWidth)                                            
-        self.setWindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowStaysOnTopHint | QtCore.Qt.WindowTransparentForInput)
+        self.resize(windowSize + 1, windowSize + 1)
+        self.pen = QtGui.QPen(QtGui.QColor(0, 128, 128, 80))
+        self.pen.setWidth(penWidth)
+        self.setWindowFlags(
+            QtCore.Qt.FramelessWindowHint
+            | QtCore.Qt.WindowStaysOnTopHint
+            | QtCore.Qt.WindowTransparentForInput
+        )
         self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
-        self.move(QtWidgets.QApplication.desktop().screen().rect().center() - self.rect().center() + QtCore.QPoint(1,1))
-
+        self.move(
+            QtWidgets.QApplication.desktop().screen().rect().center()
+            - self.rect().center()
+            + QtCore.QPoint(1, 231)
+        )
 
     def paintEvent(self, event):
         ws = self.ws
@@ -19,15 +28,16 @@ class Crosshair(QtWidgets.QWidget):
         painter = QtGui.QPainter(self)
         painter.setPen(self.pen)
         # painter.drawLine( x1,y1, x2,y2    )
-        painter.drawLine(   ws/2, 0,               ws/2, ws/2 - ws/d   )   # Top
-        painter.drawLine(   ws/2, ws/2 + ws/d,     ws/2, ws            )   # Bottom
-        painter.drawLine(   0, ws/2,               ws/2 - ws/d, ws/2   )   # Left
-        painter.drawLine(   ws/2 + ws/d, ws/2,     ws, ws/2            )   # Right
+        painter.drawPoint(ws / 2, ws / 2)
+        # painter.drawLine(ws / 2, 0, ws / 2, ws / 2 - ws / d)  # Top
+        # painter.drawLine(ws / 2, ws / 2 + ws / d, ws / 2, ws)  # Bottom
+        # painter.drawLine(0, ws / 2, ws / 2 - ws / d, ws / 2)  # Left
+        # painter.drawLine(ws / 2 + ws / d, ws / 2, ws, ws / 2)  # Right
 
 
-app = QtWidgets.QApplication(sys.argv) 
+signal.signal(signal.SIGINT, signal.SIG_DFL)
+app = QtWidgets.QApplication([])
 
-widget = Crosshair(windowSize=24, penWidth=1)
+widget = Crosshair(windowSize=25, penWidth=5)
 widget.show()
-
-sys.exit(app.exec_())
+app.exec_()
